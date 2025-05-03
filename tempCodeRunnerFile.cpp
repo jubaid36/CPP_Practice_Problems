a8b0e6bfdd2afb019c968339a071b3e3bd67f1bf
MD Jubaid <jubaid369336@gmail.com>
2025-05-04 02:27:23 +0600

    Check Even Odd

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Input the value: ";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<n<<" is Even.";
+    }else{
+        cout<<n<<" is Odd.";
+    }
+    return 0;
+}
