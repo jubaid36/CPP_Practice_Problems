ab7c23f4e8a5f652a223a7e56f9d18cee82014af
MD Jubaid <jubaid369336@gmail.com>
2025-05-03 20:21:26 +0600

    Initial commit with my C++ practice problems

---
+++ Sum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 100;
+    int sum = (n * (n + 1)) / 2;  // Sum formula
+    cout << "Sum of numbers from 1 to 100: " << sum << endl;
+    return 0;
+}
